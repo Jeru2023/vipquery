388d96d8b5433ca724dd05c6e217349f191db30e
Jeru <8791177+jeru@user.noreply.gitee.com>
2023-05-06 18:56:29 +0800

    assigned folder name to key and uuid to value

--- utils/folder_mapper.py
+++ utils/folder_mapper.py
@@ -4,24 +4,34 @@ import os.path
 
 config_path = 'folder_dict.json'
 
-def get_folder_dict():
+def get_dict():
     folder_dict = {}
     if os.path.isfile(config_path):
         with open(config_path, 'r') as f:
             folder_dict = json.load(f) 
     return folder_dict
 
-def update_dict(key, folder, folder_dict):
-    pair = {key : folder}
+def update_dict(folder, value, folder_dict):
+    pair = {folder : value}
     folder_dict.update(pair)
     return folder_dict
 
 def convert_uuid(folder):
-    # map uuid and folder name, persist localy
-    key = str(uuid.uuid4())
-    folder_dict = update_dict(key, folder, get_folder_dict())
+    """
+    Converts a given folder dictionary to a new UUID value and updates it in a JSON file.
+
+    Args:
+        folder (dict): A dictionary representing the folder to update.
+
+    Returns:
+        str: The newly generated UUID value.
+    """
+    value = str(uuid.uuid4())
+    folder_dict = update_dict(folder, value, get_dict())
     with open('folder_dict.json', 'w') as f:
         json.dump(folder_dict, f)
-    return key
+    return value
 
-convert_uuid('abc2')
+def query_uuid(folder):
+    folder_dict = get_dict()
+    return folder_dict[folder]
