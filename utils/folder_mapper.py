00ada90dfbff8a67f10b3c755df7df29ff84549e
Jeru <8791177+jeru@user.noreply.gitee.com>
2023-05-06 18:44:22 +0800

    Implemented uuid and folder name mapping logic

--- utils/folder_mapper.py
+++ utils/folder_mapper.py
@@ -1,9 +1,17 @@
 import uuid
 import json
+import os.path
 
-with open('folder_dict.json','r') as f:
-    folder_dict = json.load(f)
-def update_dict(key, folder):
+config_path = 'folder_dict.json'
+
+def get_folder_dict():
+    folder_dict = {}
+    if os.path.isfile(config_path):
+        with open(config_path, 'r') as f:
+            folder_dict = json.load(f) 
+    return folder_dict
+
+def update_dict(key, folder, folder_dict):
     pair = {key : folder}
     folder_dict.update(pair)
     return folder_dict
@@ -11,7 +19,9 @@ def update_dict(key, folder):
 def convert_uuid(folder):
     # map uuid and folder name, persist localy
     key = str(uuid.uuid4())
-    folder_dict = update_dict(key, folder)
+    folder_dict = update_dict(key, folder, get_folder_dict())
     with open('folder_dict.json', 'w') as f:
         json.dump(folder_dict, f)
     return key
+
+convert_uuid('abc2')
